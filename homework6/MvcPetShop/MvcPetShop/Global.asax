7b66b89aae012f18de0e0d1cb55cbd9efbadc672
ag993 <albingeorge@ulsecure-belknap-207115.wireless.louisville.edu>
2014-03-19 14:44:37 -0400

    Added View and COntroller

---
+++ homework6/MvcPetShop/MvcPetShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcPetShop.MvcApplication" Language="C#" %>
---
+++ homework6/MvcPetShop/MvcPetShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcPetShop.Startup))]
+namespace MvcPetShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
