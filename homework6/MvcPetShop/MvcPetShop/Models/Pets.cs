961931058d2eb0ea47d32e7390f8b81234859124
ag993 <albingeorge@ulsecure-belknap-207115.wireless.louisville.edu>
2014-03-19 14:57:14 -0400

    yay first model

---
+++ homework6/MvcPetShop/MvcPetShop/Models/Pets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+
+
+namespace MvcPetShop.Models
+{
+    public class Pets
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DoB { get; set; }
+        public string Breed { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class PetsDBContext : DbContext
+    {
+        public DbSet<Pets> Pets { get; set; }
+    }
+
+}
