3bc64eb8855bba031275a9b7caaafb29aa9892c2
Albin George <albingeorge@ulsecure-belknap-217224.wireless.louisville.edu>
2014-04-16 16:50:28 -0400

    Finished but few errors

--- hw7/ProductStore/ProductStore/Models/Order.cs
+++ hw7/ProductStore/ProductStore/Models/Order.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace ProductStore.Models
